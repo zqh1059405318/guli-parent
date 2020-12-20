2de1854e95ed5558d53f8d2283d161368646664f
zqh1059405318 <1059405318@qq.com>
2020-12-20 21:31:11 +0800

    swagger 可以按照自定义的格式进行书写了

--- service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -7,7 +7,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu"})
-@EnableSwagger2
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,9 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,7 @@ import java.util.List;
  * @since 2020-12-20
  */
 @RestController
+@Api(tags = {"讲师管理"})
 @CrossOrigin //跨域
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -25,13 +29,17 @@ public class EduTeacherController {
     private EduTeacherService teacherService;
 
     // rest风格接口
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("/findAll")
     public List<EduTeacher> findAll() {
         return teacherService.list(null);
     }
 
+    @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id) {
+    public boolean removeById(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id) {
         return teacherService.removeById(id);
     }
 }
